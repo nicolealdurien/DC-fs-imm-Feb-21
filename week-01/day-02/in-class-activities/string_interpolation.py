cc048b829c751efea609212518999786041089f9
naldurien <nicole.aldurien@gmail.com>
2021-06-19 00:03:13 -0400

    day 2 activity 1

---
+++ week-01/day-02/in-class-activities/string_interpolation.py
@@ -0,0 +1,8 @@
+# In-Class Activity: String Interpolation
+# Write an app which separately takes first name and last name of the user. Once the name is taken, print out the following message:
+
+# Hello! My name is FirstName LastName
+
+first = input("Please enter your first name: ")
+last = input("Please enter your last name: ")
+print("Hello! My name is " + first + " " + last + ".")
