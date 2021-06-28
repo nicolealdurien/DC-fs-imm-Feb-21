879a3c1b92f0828b973b04ce595e9d0ac0161d1f
naldurien <nicole.aldurien@gmail.com>
2021-06-28 11:03:55 -0400

    calculator

--- week-01/day-02/assignments/calc.py
+++ week-01/day-02/assignments/calc.py
@@ -4,7 +4,7 @@
 # ```
 # Input 1 - Represents the first number 
 
-# Input 2 - Represents the operand (+, - , * , /) 
+# Input 2 - Represents the operand (+ ,  - , * , /) 
 
 # Input 3 - Represents the second number 
 # ```
@@ -14,5 +14,37 @@
 
 # Make sure each math operation is a separate function. 
 
-def even_odd(num1, num2, operand):
-    
+def calc_input():
+    num1 = float(input("Please enter your first number: "))
+    operand = input("Please enter your operand (+ , - , * , /): ")
+    num2 = float(input("Please enter your second number: "))
+    if operand == "+":
+        add(num1, num2, operand)
+    elif operand == "-":
+        subtract(num1, num2, operand)
+    elif operand == "*":
+        multiply(num1, num2, operand)
+    elif operand == "/":
+        divide(num1, num2, operand)
+
+def add(num1, num2, operand):
+    result = num1 + num2
+    display(result)
+
+def subtract(num1, num2, operand):
+    result = num1 - num2
+    display(result)
+
+def multiply(num1, num2, operand):
+    result = num1 * num2
+    display(result)
+
+def divide(num1, num2, operand):
+    result = num1 / num2
+    display(result)
+
+def display(result):
+    str_result = str(round(result, 2))
+    print("Your result is " + str_result)
+
+calc_input()
